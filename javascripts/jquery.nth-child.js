8988b68dc47b11ae7571c8281ff0fe461be6eb7d
Masakazu Saito <slipper@slippers-MacBook-Pro.local>
2018-01-27 03:11:15 +0900

    初期コミット

---
+++ javascripts/jquery.nth-child.js
@@ -0,0 +1,5 @@
+(function($) {
+    $(".top3 li:nth-child(n+4)").css("display","none");
+    $(".top2 article:nth-child(n+3)").css("display","none");
+    $("article:last-child").css("border-bottom","none");
+}(jQuery));
