ac14cd3b15dfd53e3f66130fac778b65c74e98da
Masakazu Saito <slipper@slippers-MacBook-Pro.local>
2018-08-24 21:02:07 +0900

    AINI-IKA-NIGHT 終了後

--- javascripts/jquery.nth-child.js
+++ javascripts/jquery.nth-child.js
@@ -1,5 +1,5 @@
 (function($) {
     $(".top3 li:nth-child(n+4)").css("display","none");
-    $(".top2 article:nth-child(n+3)").css("display","none");
+    $(".top3 article:nth-child(n+5)").css("display","none");
     $("article:last-child").css("border-bottom","none");
 }(jQuery));
